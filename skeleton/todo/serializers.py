3523d37a3dae6e858dc808bc20871b1ebd300c6f
chiller <egalaczi_business@yahoo.com>
2013-03-26 21:01:29 +0100

    min version

--- skeleton/todo/serializers.py
+++ skeleton/todo/serializers.py
@@ -1,14 +1,3 @@
-from rest_framework import serializers
-from models import  Todo
-
-class TodoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Todo
-
-    def restore_fields(self, data, files):
-        deserialized = super(TodoSerializer, self).restore_fields(data, files)
-        #TODO: whatever you feel like doing before it gets saved
-        return deserialized
 
 
 
--- skeleton/todo/views.py
+++ skeleton/todo/views.py
@@ -1,15 +1,2 @@
 from models import Todo
 from rest_framework import generics
-from rest_framework.decorators import api_view
-from rest_framework.mixins import DestroyModelMixin
-from rest_framework.reverse import reverse
-from rest_framework.response import Response
-from serializers import TodoSerializer
-
-class TodoView(generics.ListCreateAPIView):
-    model = Todo
-    serializer_class = TodoSerializer
-
-class TodoDetailView(generics.RetrieveUpdateDestroyAPIView):
-    model = Todo
-    serializer_class = TodoSerializer
